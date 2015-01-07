80070158ad86cc6cbdef06a7d2ef551e40c71866
framlin <w.egger@framlin.com>
2015-01-07 18:21:57 +0100

    make partials more fine grained

--- builder.js
+++ builder.js
@@ -1,6 +1,5 @@
 var restify = require('restify'),
 
-    PARTIAL_COUNT = 5,
     cvClient = restify.createClient({
         url: 'http://127.0.0.1:8089'
     }),
@@ -10,18 +9,25 @@ var restify = require('restify'),
 
 
 
-function Builder() {
+function Builder(sitename) {
+    console.log(sitename)
     var me = this;
     this.partialIndex = {
-        top: '',
-        bottom: '',
-        showcases: '',
-        disclaimer: '',
-        impressum: ''
+        'top' : "",
+        'heading' : "",
+        'topnav' : "",
+        'maintop' : "",
+        'impressum': "",
+        'bottomnav': "",
+        'disclaimer': "",
+        'metanav': "",
+        'mainbottom': "",
+        'bottom' : ""
     };
 
     this.loadPartials = function loadPartials(cbDone) {
-        var partialCount = PARTIAL_COUNT;
+        var partialCount = 0,
+            partial;
 
         function finalize() {
             partialCount -= 1;
@@ -46,25 +52,19 @@ function Builder() {
             };
         }
 
-        ciClient.get('/ci/partial/top',function cbCIHandling(err, req) {
-            req.on('result', createPartialGetter(me.partialIndex, 'top'));
-        });
+        function crateCIGetter(partial) {
+            return function cbCIHandling(err, req) {
+                req.on('result', createPartialGetter(me.partialIndex, partial));
+            };
+        }
 
-        ciClient.get('/ci/partial/bottom',function cbCIHandling(err, req) {
-            req.on('result', createPartialGetter(me.partialIndex, 'bottom'));
-        });
+        for (partial in this.partialIndex) {
+            if (this.partialIndex[partial] === "") {
+                partialCount += 1;
+                ciClient.get('/ci/'+sitename+'/partial/'+partial, crateCIGetter(partial));
+            }
+        }
 
-        ciClient.get('/ci/partial/showcases',function cbCIHandling(err, req) {
-            req.on('result', createPartialGetter(me.partialIndex, 'showcases'));
-        });
-
-        ciClient.get('/ci/partial/disclaimer',function cbCIHandling(err, req) {
-            req.on('result', createPartialGetter(me.partialIndex, 'disclaimer'));
-        });
-
-        ciClient.get('/ci/partial/impressum',function cbCIHandling(err, req) {
-            req.on('result', createPartialGetter(me.partialIndex, 'impressum'));
-        });
     };
 
     this.createCIHandler = function createCIHandler(ctx, mimeType, encoding) {
@@ -106,13 +106,18 @@ function Builder() {
                     'Content-Type': 'text/html'
                 });
                 ctx.res.write(me.partialIndex.top);
+                ctx.res.write(me.partialIndex.heading);
+                ctx.res.write(me.partialIndex.maintop);
+                ctx.res.write(me.partialIndex.topnav);
                 res.setEncoding('utf8');
                 res.on('data', function(chunk) {
                     ctx.res.write(chunk);
                 });
 
                 res.on('end', function() {
-                    ctx.res.write(me.partialIndex.showcases);
+                    ctx.res.write(me.partialIndex.bottomnav);
+                    ctx.res.write(me.partialIndex.metanav);
+                    ctx.res.write(me.partialIndex.mainbottom);
                     ctx.res.write(me.partialIndex.bottom);
                     ctx.res.end();
                 });
@@ -129,15 +134,15 @@ function Builder() {
     };
 
     this.ciStyle = function ciStyle(ctx, stylePath) {
-        ciClient.get('/ci/style/'+stylePath, this.createCIHandler(ctx, 'text/css', 'utf8'));
+        ciClient.get('/ci/'+sitename+'/style/'+stylePath, this.createCIHandler(ctx, 'text/css', 'utf8'));
     };
 
     this.ciPartial = function ciPartial(ctx, partialPath) {
-        ciClient.get('/ci/partial/'+partialPath, this.createCIHandler(ctx, 'text/html', 'utf8'));
+        ciClient.get('/ci/'+sitename+'/partial/'+partialPath, this.createCIHandler(ctx, 'text/html', 'utf8'));
     };
 
     this.ciImage = function ciImage(ctx, imagePath) {
-        ciClient.get('/ci/image/'+imagePath, this.createCIHandler(ctx, 'image/png'));
+        ciClient.get('/ci/'+sitename+'/image/'+imagePath, this.createCIHandler(ctx, 'image/png'));
     };
 
     this.ciFont = function ciFont(ctx, fontPath) {
@@ -153,8 +158,13 @@ function Builder() {
             'Content-Type': 'text/html'
         });
         this.res.write(me.partialIndex.top);
+        this.res.write(me.partialIndex.heading);
+        this.res.write(me.partialIndex.maintop);
+        this.res.write(me.partialIndex.topnav);
         this.res.write(me.partialIndex.impressum);
-        this.res.write(me.partialIndex.showcases);
+        this.res.write(me.partialIndex.bottomnav);
+        this.res.write(me.partialIndex.metanav);
+        this.res.write(me.partialIndex.mainbottom);
         this.res.write(me.partialIndex.bottom);
         this.res.end();
     };
@@ -167,8 +177,13 @@ function Builder() {
             'Content-Type': 'text/html'
         });
         this.res.write(me.partialIndex.top);
+        this.res.write(me.partialIndex.heading);
+        this.res.write(me.partialIndex.maintop);
+        this.res.write(me.partialIndex.topnav);
         this.res.write(me.partialIndex.disclaimer);
-        this.res.write(me.partialIndex.showcases);
+        this.res.write(me.partialIndex.bottomnav);
+        this.res.write(me.partialIndex.metanav);
+        this.res.write(me.partialIndex.mainbottom);
         this.res.write(me.partialIndex.bottom);
         this.res.end();
     };
@@ -177,4 +192,4 @@ function Builder() {
 
 
 
-module.exports = new Builder();
+module.exports = Builder;
