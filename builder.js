5287686c31e67ffe9d53d42b4ef88c3c2c131bb5
framlin <w.egger@framlin.com>
2015-01-10 22:40:02 +0100

    auto-overwrite local partials naming

--- builder.js
+++ builder.js
@@ -16,9 +16,12 @@ var restify = require('restify'),
     });
 
 
-function Builder(sitename) {
-    console.log(sitename)
-    var me = this;
+function Builder(localPartials) {
+    var me = this,
+        partial;
+
+
+    this.sitename = 'company';
 
     this.partialIndex = {
         'top' : "",
@@ -31,22 +34,28 @@ function Builder(sitename) {
         'bottom' : ""
     };
 
+    //overwrite framlin base CI
+    for (partial in localPartials) {
+        this.partialIndex[partial] = localPartials[partial];
+    }
+
     this.articleIndex = {};
 
-    this.loadContent = function loadContent(cbDone) {
-        this.loadPartials(function cbPartialsLoaded(){
-            me.loadArticles(cbDone);
+    this.loadContent = function loadContent(sitename, next) {
+        me.sitename = sitename;
+        me.loadPartials(function cbPartialsLoaded(){
+            me.loadArticles(next);
         });
     };
 
-    this.loadArticles = function loadArticles(cbDone) {
+    this.loadArticles = function loadArticles(next) {
         var articleCount = 0,
             article;
 
         function finalize() {
             articleCount -= 1;
             if (articleCount === 0) {
-                cbDone();
+                next();
             }
         }
 
@@ -117,20 +126,20 @@ function Builder(sitename) {
             });
         }
 
-        coClient.get('/co/'+sitename+'/index/article', coIndexHandler);
+        coClient.get('/co/'+me.sitename+'/index/article', coIndexHandler);
     };
 
 
 
 
-    this.loadPartials = function loadPartials(cbDone) {
+    this.loadPartials = function loadPartials(next) {
         var partialCount = 0,
             partial;
 
         function finalize() {
             partialCount -= 1;
             if (partialCount === 0) {
-                cbDone();
+                next();
             }
         }
 
@@ -159,18 +168,18 @@ function Builder(sitename) {
         for (partial in this.partialIndex) {
             if (this.partialIndex[partial] === "") {
                 partialCount += 1;
-                ciClient.get('/ci/'+sitename+'/partial/'+partial, crateCIGetter(partial));
+                ciClient.get('/ci/'+me.sitename+'/partial/'+partial, crateCIGetter(partial));
             }
         }
 
     };
 
-    this.createCIHandler = function createCIHandler(ctx, mimeType, encoding) {
+    this.createCIHandler = function createCIHandler(router, mimeType, encoding) {
         return function cbCIHandling(err, req) {
             req.on('result', function(err, res) {
                 //assert.ifError(err); // HTTP status code >= 400
 
-                ctx.res.writeHead(200, {
+                router.res.writeHead(200, {
                     'Content-Type': mimeType
                 });
 
@@ -179,11 +188,11 @@ function Builder(sitename) {
                         res.setEncoding(encoding);
                     }
                     res.on('data', function(chunk) {
-                        ctx.res.write(chunk);
+                        router.res.write(chunk);
                     });
 
                     res.on('end', function() {
-                        ctx.res.end();
+                        router.res.end();
                     });
                 } else {
                     console.log(err);
@@ -195,79 +204,80 @@ function Builder(sitename) {
     }
 
 
-    this.page = function  page(ctx)  {
+    this.page = function  page(router)  {
         return function serviceRequest(err, req ) {
             req.on('result', function(err, res) {
                 //assert.ifError(err); // HTTP status code >= 400
 
-                ctx.res.writeHead(200, {
+                router.res.writeHead(200, {
                     'Content-Type': 'text/html'
                 });
-                ctx.res.write(me.partialIndex.top);
-                ctx.res.write(me.partialIndex.heading);
-                ctx.res.write(me.partialIndex.maintop);
-                ctx.res.write(me.partialIndex.topnav);
+                router.res.write(me.partialIndex.top);
+                router.res.write(me.partialIndex.heading);
+                router.res.write(me.partialIndex.maintop);
+                router.res.write(me.partialIndex.topnav);
                 res.setEncoding('utf8');
                 res.on('data', function(chunk) {
-                    ctx.res.write(chunk);
+                    router.res.write(chunk);
                 });
 
                 res.on('end', function() {
-                    ctx.res.write(me.partialIndex.bottomnav);
-                    ctx.res.write(me.partialIndex.metanav);
-                    ctx.res.write(me.partialIndex.mainbottom);
-                    ctx.res.write(me.partialIndex.bottom);
-                    ctx.res.end();
+                    router.res.write(me.partialIndex.bottomnav);
+                    router.res.write(me.partialIndex.metanav);
+                    router.res.write(me.partialIndex.mainbottom);
+                    router.res.write(me.partialIndex.bottom);
+                    router.res.end();
                 });
             });
         };
     };
 
-    this.cv = function cv(ctx) {
-        cvClient.get('/cv/list', this.page(ctx));
+    this.cv = function cv(router) {
+        cvClient.get('/cv/list', this.page(router));
     };
 
-    this.cvEntry = function cvEntry(ctx, cvid) {
-        cvClient.get('/cv/'+ cvid, this.page(ctx));
+    this.cvEntry = function cvEntry(router, cvid) {
+        cvClient.get('/cv/'+ cvid, this.page(router));
     };
 
-    this.coArticle = function coArticle(ctx, site, articlePath) {
-        coClient.get('/co/'+site+'/article/'+articlePath, this.page(ctx));
+    this.coArticle = function coArticle(router, site, articlePath) {
+        coClient.get('/co/'+site+'/article/'+articlePath, this.page(router));
     };
 
-    this.ciStyle = function ciStyle(ctx, site, stylePath) {
-        ciClient.get('/ci/'+site+'/style/'+stylePath, this.createCIHandler(ctx, 'text/css', 'utf8'));
+    this.ciStyle = function ciStyle(router, site, stylePath) {
+        ciClient.get('/ci/'+site+'/style/'+stylePath, this.createCIHandler(router, 'text/css', 'utf8'));
     };
 
-    this.ciPartial = function ciPartial(ctx, partialPath) {
-        ciClient.get('/ci/'+sitename+'/partial/'+partialPath, this.createCIHandler(ctx, 'text/html', 'utf8'));
+    this.ciPartial = function ciPartial(router, partialPath) {
+        ciClient.get('/ci/'+me.sitename+'/partial/'+partialPath, this.createCIHandler(router, 'text/html', 'utf8'));
     };
 
-    this.ciImage = function ciImage(ctx, imagePath) {
-        ciClient.get('/ci/'+sitename+'/image/'+imagePath, this.createCIHandler(ctx, 'image/png'));
+    this.ciImage = function ciImage(router, imagePath) {
+        ciClient.get('/ci/'+me.sitename+'/image/'+imagePath, this.createCIHandler(router, 'image/png'));
     };
 
-    this.ciFont = function ciFont(ctx, fontPath) {
-        ciClient.get('/ci/font/'+fontPath, this.createCIHandler(ctx, 'application/x-font-opentype'));
+    this.ciFont = function ciFont(router, fontPath) {
+        ciClient.get('/ci/font/'+fontPath, this.createCIHandler(router, 'application/x-font-opentype'));
     };
 
     /**
      * @callback: 'this' is the calling router
      */
     this.home = function home() {
-        this.res.writeHead(200, {
+        var router = this;
+        router.res.writeHead(200, {
             'Content-Type': 'text/html'
         });
-        this.res.write(me.partialIndex.top);
-        this.res.write(me.partialIndex.heading);
-        this.res.write(me.partialIndex.maintop);
-        this.res.write(me.partialIndex.topnav);
-        this.res.write(me.articleIndex.home);
-        this.res.write(me.partialIndex.bottomnav);
-        this.res.write(me.partialIndex.metanav);
-        this.res.write(me.partialIndex.mainbottom);
-        this.res.write(me.partialIndex.bottom);
-        this.res.end();
+        router.res.write(me.partialIndex.top);
+        router.res.write(me.partialIndex.heading);
+        router.res.write(me.partialIndex.maintop);
+        router.res.write(me.partialIndex.topnav);
+        router.res.write(me.articleIndex.home);
+        router.res.write(me.partialIndex.bottomnav);
+        router.res.write(me.partialIndex.metanav);
+        router.res.write(me.partialIndex.mainbottom);
+        router.res.write(me.partialIndex.bottom);
+        router.res.end();
     };
 
 
@@ -275,56 +285,59 @@ function Builder(sitename) {
      * @callback: 'this' is the calling router
      */
     this.impressum = function impressum() {
-        this.res.writeHead(200, {
+        var router = this;
+        router.res.writeHead(200, {
             'Content-Type': 'text/html'
         });
-        this.res.write(me.partialIndex.top);
-        this.res.write(me.partialIndex.heading);
-        this.res.write(me.partialIndex.maintop);
-        this.res.write(me.partialIndex.topnav);
-        this.res.write(me.articleIndex.impressum);
-        this.res.write(me.partialIndex.bottomnav);
-        this.res.write(me.partialIndex.metanav);
-        this.res.write(me.partialIndex.mainbottom);
-        this.res.write(me.partialIndex.bottom);
-        this.res.end();
+        router.res.write(me.partialIndex.top);
+        router.res.write(me.partialIndex.heading);
+        router.res.write(me.partialIndex.maintop);
+        router.res.write(me.partialIndex.topnav);
+        router.res.write(me.articleIndex.impressum);
+        router.res.write(me.partialIndex.bottomnav);
+        router.res.write(me.partialIndex.metanav);
+        router.res.write(me.partialIndex.mainbottom);
+        router.res.write(me.partialIndex.bottom);
+        router.res.end();
     };
 
     /**
      * @callback: 'this' is the calling router
      */
     this.disclaimer = function disclaimer() {
-        this.res.writeHead(200, {
+        var router = this;
+        router.res.writeHead(200, {
             'Content-Type': 'text/html'
         });
-        this.res.write(me.partialIndex.top);
-        this.res.write(me.partialIndex.heading);
-        this.res.write(me.partialIndex.maintop);
-        this.res.write(me.partialIndex.topnav);
-        this.res.write(me.articleIndex.disclaimer);
-        this.res.write(me.partialIndex.bottomnav);
-        this.res.write(me.partialIndex.metanav);
-        this.res.write(me.partialIndex.mainbottom);
-        this.res.write(me.partialIndex.bottom);
-        this.res.end();
+        router.res.write(me.partialIndex.top);
+        router.res.write(me.partialIndex.heading);
+        router.res.write(me.partialIndex.maintop);
+        router.res.write(me.partialIndex.topnav);
+        router.res.write(me.articleIndex.disclaimer);
+        router.res.write(me.partialIndex.bottomnav);
+        router.res.write(me.partialIndex.metanav);
+        router.res.write(me.partialIndex.mainbottom);
+        router.res.write(me.partialIndex.bottom);
+        router.res.end();
     };
     /**
      * @callback: 'this' is the calling router
      */
     this.about = function about() {
-        this.res.writeHead(200, {
+        var router = this;
+        router.res.writeHead(200, {
             'Content-Type': 'text/html'
         });
-        this.res.write(me.partialIndex.top);
-        this.res.write(me.partialIndex.heading);
-        this.res.write(me.partialIndex.maintop);
-        this.res.write(me.partialIndex.topnav);
-        this.res.write(me.articleIndex.about);
-        this.res.write(me.partialIndex.bottomnav);
-        this.res.write(me.partialIndex.metanav);
-        this.res.write(me.partialIndex.mainbottom);
-        this.res.write(me.partialIndex.bottom);
-        this.res.end();
+        router.res.write(me.partialIndex.top);
+        router.res.write(me.partialIndex.heading);
+        router.res.write(me.partialIndex.maintop);
+        router.res.write(me.partialIndex.topnav);
+        router.res.write(me.articleIndex.about);
+        router.res.write(me.partialIndex.bottomnav);
+        router.res.write(me.partialIndex.metanav);
+        router.res.write(me.partialIndex.mainbottom);
+        router.res.write(me.partialIndex.bottom);
+        router.res.end();
     };
 }
 
